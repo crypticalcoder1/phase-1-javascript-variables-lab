a330ba2e4eafd22f89d328f0f1b9e0add57571fb
crypticalcoder1 <kirambawycliff@gmail.com>
2024-07-01 08:05:01 +0300

    my work

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+// index.js
+
+// companyName should be declared as a const and set to 'Scuber'
+const companyName = 'Scuber';
+
+// mostProfitableNeighborhood should be declared as a let and set to 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// companyCeo should be declared as a let and set to 'Susan Smith'
+let companyCeo = 'Susan Smith';
+
+// Exporting variables for testing purposes if required
+module.exports = {
+  companyName,
+  mostProfitableNeighborhood,
+  companyCeo
+};
